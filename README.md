07477b620b2fe4aeb33c73620c623cd1b931c9d5
clinzy25 <conner.linzy25@gmail.com>
2021-08-01 10:53:56 -0400

    clean up react files, update readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Live at: https://time-blocker.netlify.app/
 
+Create a .env and add your Auth0 credentials
+
 In project root:
 - npm install
 - npm start
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { TableProvider } from './reducers-contexts/table_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
@@ -21,8 +20,3 @@ ReactDOM.render(
   </Auth0Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
